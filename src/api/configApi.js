8ab2a7857ccfa9dbac28742f4b0d7bbf72315d53
Mayconyrp <csa.maycon@gmail.com>
2023-06-05 23:55:27 -0300

    instruindo no uso da url

--- src/api/configApi.js
+++ src/api/configApi.js
@@ -1,3 +1,4 @@
 //Arquivo com URL da api
-
-export const BASE_URL = 'http://192.168.0.107:8080';
+//no 'localhost' ponha o ip da máquina que está rodando a aplicação back-end
+// é necessário que os aparaelhos estejam conectados na mesma rede ( em caso do back estar rodando em localhost)
+export const BASE_URL = 'http://localhost:8080';
